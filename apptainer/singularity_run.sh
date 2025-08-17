997886b3ceca7607cc9a75288a5cebda569078e4
ry0hei-kobayashi <kobayashi.ryohei621@mail.kyutech.jp>
2025-08-17 18:39:16 +0900

    mod: pi0 eval env

--- apptainer/singularity_run.sh
+++ apptainer/singularity_run.sh
@@ -6,7 +6,16 @@ else
   exit 1
 fi
 
+#singularity run --nvccli \
 singularity run --nv \
     --bind /tmp/.X11-unix:/tmp/.X11-unix \
     --bind ~/vla/lerobot:/root/lerobot \
-    pi0.sif
+    pi0.sif \
+    python /root/lerobot/lerobot/scripts/eval.py --pretrained_policy.path=/root/lerobot/lerobot/apptainer/outputs/train/2025-05-18/02-33-56_pi0/checkpoints/last/pretrained_model
+
+#echo "train starting"
+#python /root/lerobot/lerobot/scripts/train.py \
+#    --policy.path=lerobot/pi0 \
+#    --dataset.repo_id=danaaubakirova/koch_test
+#echo "train finished"
+
